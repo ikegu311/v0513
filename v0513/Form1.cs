bb3e55be04794f1fb0fbe1946b81a2f4ebb11ee1
fusatoshi3104 <satoshidat@gmail.com>
2021-05-13 11:16:22 +0900

    エラーの確認

--- v0513/Form1.cs
+++ v0513/Form1.cs
@@ -16,5 +16,11 @@ namespace v0513
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            MessageBox.Show("" + 10 / a);
+        }
     }
 }
